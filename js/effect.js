31e8de3eeed0211a897e51508bd8e699a068b461
weemooo <flymetothelune@gmail.com>
2015-07-07 21:11:45 +0800

    WIP on TGBH

---
+++ js/effect.js
@@ -0,0 +1,25 @@
+//	Pseudo Codes
+
+//	FIRST LOAD EFFECT
+//	1. Grab top-bar
+//	     Hide for 2 seconds
+//	2. Grab wallpaper
+//	     Slide up after 2 seconds (timeout function?)
+//	3. All content slide up
+
+//	VIDEO EFFECT
+//	1. Initial
+//	     Stop and mute
+//	2. In focus
+//	     Start playing
+//	     un-mute button
+
+//	CHARACTERS EFFECTS
+//	2. on hover
+//	     light up
+//	1. on click
+//	     display short description on top
+
+//	TRANSITION EFFECTS
+//	1. Fly in Fly Out
+//	2. Create depth
